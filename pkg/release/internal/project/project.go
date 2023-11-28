8036e2e111344e1fd17f7b1e5d0a6a72d8d140d0
laurentsimon <laurentsimon@google.com>
2023-11-28 23:05:01 +0000

    remove comment
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- pkg/release/internal/project/project.go
+++ pkg/release/internal/project/project.go
@@ -149,34 +149,4 @@ func FromReaders(readers iterator.ReaderIterator, orgPolicy organization.Policy)
 		return nil, fmt.Errorf("failed to read policy: %w", readers.Error())
 	}
 	return policies, nil
-	// TODO: use URI + environment to key the structure:
-	// URI_env1, URI_env2, etc. Look up will be faster.
-	// And we can easily tell if there's overlap or not.
-	// if env is empty, we'll...
-	// need a map[URI_env] = policy
-	// TODO: artifact + dev must come frm the same folder?
-	// I think we only want once, taht's it. if use env,
-	// must be in a sigle file anyway.
-	// var policies []Policy
-	// err := filepath.Walk(".",
-	// 	func(path string, info os.FileInfo, err error) error {
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 		absPath, err := filepath.Abs(path)
-	// 		if err != nil {
-	// 			return fmt.Errorf("failed to read absolute policy path: %w", err)
-	// 		}
-	// 		if absPath == orgPolicy.Path() {
-	// 			return nil
-	// 		}
-	//		files = append(files, absPath)
-	// 		fmt.Println(path, info.Size())
-
-	// 		return nil
-	// 	})
-	// validate for unique artifact across all files.
-	// use orgPolicy.Path()
-	// use RootBuilderNames()
-	//return nil, nil
 }
