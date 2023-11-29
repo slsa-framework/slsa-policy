2688cd9402df84fee808ebf1aeb5a4bbb5c0106a
laurentsimon <laurentsimon@google.com>
2023-11-29 23:03:13 +0000

    remove comments
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- pkg/release/release.go
+++ pkg/release/release.go
@@ -1,7 +1,6 @@
 package release
 
 import (
-	"fmt"
 	"io"
 
 	"github.com/laurentsimon/slsa-policy/pkg/release/internal"
@@ -25,22 +24,7 @@ func New(org io.ReadCloser, projects iterator.ReadCloserIterator) (*Policy, erro
 	}, nil
 }
 
-func Hello() error {
-	fmt.Println("hey")
-	return nil
+// Evaluate evalues the release policy.
+func (p *Policy) Evaluate(publicationURI string, buildOpts options.BuildVerification) (int, error) {
+	return p.Evaluate(publicationURI, buildOpts)
 }
-
-func (p *Policy) Evaluate(publicationURI string, buildConfig options.BuildVerificationConfig) (int, error) {
-	return p.Evaluate(publicationURI, buildConfig)
-}
-
-// TODO:
-// https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
-// Evaluate evaluates the policy.
-// func (p *Policy) Evaluate(options ...Options) error {
-// 	return p.policy.Evaluate(sourceURI, imageURI, builderID)
-// }
-
-// func (p *Policy) Evaluate() results.Verification {
-// 	return p.policy.Evaluate(sourceURI, imageURI, builderID)
-// }
